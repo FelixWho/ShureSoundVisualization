cfe1d367d9adf8a5ee6fecb3e02d66d9d3bde827
TimDSF <TimDSF@Outlook.com>
2019-10-26 23:32:00 -0500

    Tim Pushes

--- tim-py-fft/main.py
+++ tim-py-fft/main.py
@@ -1,39 +1,38 @@
 import scipy.io.wavfile
 import numpy as np
+import matplotlib.pyplot as plt
 
-from fft import fft
+from util import util
 
+# --------------------------------------------------------------- full audio
+# loading the full audio sequence
+rate, data_seq = scipy.io.wavfile.read("test/pianoFull.wav")
+data_seq = data_seq[:,0]
+l_seq = len(data_seq)
+
+# peek detection for the full audio sequence
+peak_seq = util.findPeak(data_seq, display=False)
+
+# ---------------------------------------------------------------- detected sequence
 # loading the received audio chunk
 rate, data_chunk = scipy.io.wavfile.read('test/piano50.wav')
 data_chunk = data_chunk[:,0]
 l_chunk = len(data_chunk)
 
-mean_chunk = 0;
-for i in data_chunk:
-	mean_chunk += i
-mean_chunk /= l_chunk
-
-# loading the original audio sequence
-rate, data_seq = scipy.io.wavfile.read("test/pianoFull.wav")
-data_seq = data_seq[:,0]
-l_seq = len(data_seq)
-
-mean_seq = 0;
-for i in data_seq:
-	mean_seq += i
-mean_seq /= l_seq
+# peek detection for the recorded audio sequence
+peak_chunk = util.findPeak(rate, data_chunk, display=False)
 
 # plotting the wave figure and the Fourier Transformation figure
-t = np.linspace(0,l_chunk/rate,l_chunk)
-X, freqs = fft.sigFreq(t, rate, data_chunk, plot=True)
+X, freqs = util.ttf(rate, data_chunk, plot=False)
 
-# brutal forcing the optimal match difference
-min_diff = 0x7fffffff;
-for i in range(0, l_seq-l_chunk, 10):
-	tmp = 0
-	for j in range (0, l_chunk, 10):
-		tmp += abs(data_seq[i+j]-data_chunk[j])
-	if (tmp < min_diff): min_diff = tmp;
+# match_pos = util.match(rate, peak_seq, peak_chunk)
 
-print(min_diff);
+# # brutal forcing the optimal match difference
+# min_diff = 0x7fffffff;
+# for i in range(0, l_seq-l_chunk, 10):
+# 	tmp = 0
+# 	for j in range (0, l_chunk, 10):
+# 		tmp += abs(data_seq[i+j]-data_chunk[j])
+# 	if (tmp < min_diff): min_diff = tmp;
 
+# print(min_diff);
